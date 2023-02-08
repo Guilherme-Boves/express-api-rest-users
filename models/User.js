67cb5c09afa2ea007e0cdc55479a446f8f9cbd3f
Guilherme Boves <ghboves@gmail.com>
2023-02-08 11:14:32 -0300

    Rotina de exclusão de usuários

--- models/User.js
+++ models/User.js
@@ -104,6 +104,29 @@ class User {
             }
         }
     }
+
+    async delete(id){
+        var user = await this.findById(id);
+
+        if(user != undefined){
+
+            try{
+                await knex.delete().where({id: id}).table("users");
+                return {status: true}
+            } catch(err){
+                return {
+                    status: false, 
+                    err: err
+                }
+            }
+
+        } else {
+            return {
+                status: false, 
+                err: "O usuário não existe!"
+            }
+        }
+    }
 }
 
 module.exports = new User();
